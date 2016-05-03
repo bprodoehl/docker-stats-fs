e988610bdbf18643b62e2de1a715e7633cea509e
Brian Prodoehl <bprodoehl@connectify.me>
2016-05-03 14:19:57 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # docker-stats-fs
+
+This library is based on the docker-stats library written by
+Peter Elger <elger.peter@gmail.com> (http://peterelger.com/) and
+Matteo Collina <matteo.collina@nearform.com>, but it draws the statistics
+direct from the filesystem, as opposed to using the stats endpoint in the
+Docker API.  This allows it to be selective about what stats to include, which
+allows it to work comfortably with many, many running containers.
---
+++ stats.js
@@ -0,0 +1,216 @@
+#!/usr/bin/env node
+
+'use strict';
+
+var async = require('async');
+var fs = require('fs');
+var through = require('through2');
+//var split = require('split2');
+//var pump = require('pump');
+var allContainers = require('docker-allcontainers');
+
+// This function is from https://github.com/soldair/node-procfs-stats
+function sectiontable(buf) {
+  if (!buf) { return false; }
+  var lines = buf.toString().trim().split('\n');
+  var sections = lines.shift();
+
+  var columns = lines.shift().trim().split('|');
+  sections = sections.split('|');
+
+  var s,l,c,p = 0,map = {},keys = [];
+  for (var i=0; i<sections.length; ++i) {
+    s = sections[i].trim();
+    l = sections[i].length;
+    c = columns[i].trim().split(/\s+/g);
+    while(c.length) {
+      map[keys.length] = s;
+      keys.push(c.shift());
+    }
+    p += s.length+1;
+  }
+
+  var data = [];
+  lines.forEach(function(l) {
+    l = l.trim().split(/\s+/g);
+    var o = {};
+    for (var i=0; i<l.length; ++i) {
+      var s = map[i];
+      // Inter-|
+      // face  |
+      /*
+        {
+          "Interface":"eth0"
+        }
+      */
+      if (s.indexOf('-') === s.length-1) {
+        o[s.substr(s,s.length-1)+keys[i]] = l[i];
+      } else {
+        /*
+        {
+          bytes:{
+            Recieve:43124236,
+            Transmit:87782782
+          }
+        }
+        */
+        if (!o[keys[i]]) {
+          o[keys[i]] = {};
+        }
+        o[keys[i]][s] = l[i];
+      }
+    }
+    data.push(o);
+  });
+
+  return data;
+}
+
+function stats(opts) {
+  opts = opts || {};
+  var result = through.obj();
+  var events = opts.events || allContainers(opts);
+  var containers = {};
+  var oldDestroy = result.destroy;
+  var interval = opts.statsinterval || 1;
+  var statsPullInProgress = false;
+  var procPath = opts.procPath || '/proc/';
+
+  var netStats = opts.net || false;
+
+  function detachContainer(id) {
+    if (containers[id]) {
+      delete containers[id];
+    }
+  }
+
+  function attachContainer(data, container) {
+    // we are trying to tap into this container
+    // we should not do that, or we might be stuck in
+    // an output loop
+    if (data.id.indexOf(process.env.HOSTNAME) === 0) {
+      return;
+    }
+    // we need the pid for network stats
+    container.inspect(function (err, details) {
+      if (!err) {
+        //console.log(details);
+        data.info = details;
+        containers[data.id] = {meta: data, docker: container};
+      }
+    });
+  }
+
+  // this function takes the /proc/net/dev format and converts it to match
+  function transformProcToDocker(data, stats) {
+    for (var idx in data) {
+      var iface = data[idx];
+      //console.log('Looking at ' + JSON.stringify(iface));
+      if (!stats.networks) { stats.networks = {}; }
+      stats.networks[iface.Interface] = {
+        'rx_bytes': iface.bytes.Receive,
+        'rx_dropped': iface.drop.Receive,
+        'rx_errors': iface.errs.Receive,
+        'rx_packets': iface.packets.Receive,
+        'tx_bytes': iface.bytes.Transmit,
+        'tx_dropped': iface.drop.Transmit,
+        'tx_errors': iface.errs.Transmit,
+        'tx_packets': iface.packets.Transmit
+      };
+    }
+  }
+
+  function getContainerStats(container, next) {
+    var containerObj = containers[container];
+    if (containerObj && containerObj.docker) {
+      var containerPid = 0;
+      if (containerObj.meta.info && containerObj.meta.info.State) {
+        containerPid = containerObj.meta.info.State.Pid;
+      }
+      //console.log('Pulling stats on ' + JSON.stringify(containerObj.meta));//containerObj.Pid);
+      // console.log('Pulling stats on ' + containerObj.meta.id +
+      //             ' (pid ' + containerPid + ')');
+      if (netStats && containerPid) {
+        //console.log('Looking for network stats');
+        fs.readFile(procPath + containerPid + '/net/dev', function(err, buf) {
+          if (err) {
+            console.log('Error fetching network stats: ' + JSON.stringify(err));
+          } else {
+            var data = sectiontable(buf);
+            //console.log(JSON.stringify(data));
+            var stats = {};
+            transformProcToDocker(data, stats);
+            result.push({
+              v: 0,
+              id: container.slice(0, 12),
+              image: containerObj.meta.image,
+              name: containerObj.meta.name,
+              stats: stats
+            });
+          }
+          next();
+        });
+      } else {
+        console.log('no pid for that one!');
+        next();
+      }
+    } else {
+      next();
+    }
+  }
+
+  function getAllContainerStats() {
+    if (statsPullInProgress) {
+      console.log('Skipping, because pull is already in progress!');
+      return;
+    }
+    statsPullInProgress = true;
+    async.eachSeries(Object.keys(containers),
+                     async.ensureAsync(getContainerStats), function() {
+      statsPullInProgress = false;
+    });
+  }
+
+  result.setMaxListeners(0);
+
+  result.destroy = function() {
+    console.log('Destroying result stream.');
+    Object.keys(containers).forEach(detachContainer);
+    events.destroy();
+    oldDestroy.call(this);
+  };
+
+  events.on('start', attachContainer);
+  events.on('stop', function(meta) {
+    detachContainer(meta.id);
+  });
+
+  setInterval(getAllContainerStats, interval*1000);
+
+  return result;
+}
+
+module.exports = stats;
+
+function cli() {
+  var argv = require('minimist')(process.argv.slice(2));
+  stats({
+    statsinterval: argv.statsinterval,
+    matchByName: argv.matchByName,
+    matchByImage: argv.matchByImage,
+    skipByName: argv.skipByName,
+    skipByImage: argv.skipByImage,
+    procPath: argv.procPath,
+    cpuStats: argv.cpu,
+    ioStats: argv.io,
+    netStats: argv.net
+  }).pipe(through.obj(function(chunk, enc, cb) {
+    this.push(JSON.stringify(chunk));
+    this.push('\n');
+    cb();
+  })).pipe(process.stdout);
+}
+
+if (require.main === module) {
+  cli();
+}
